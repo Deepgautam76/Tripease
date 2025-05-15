d0778e044645b20c62744b1a4e9b679a2c58d047
Kamal <kamaldeep734833@gmail.com>
2025-05-16 00:17:13 +0530

    build the customer transformer

--- src/main/java/com/deep/tripease/transformer/CustomerDtoTransformer.java
+++ src/main/java/com/deep/tripease/transformer/CustomerDtoTransformer.java
@@ -1,5 +1,26 @@
 package com.deep.tripease.transformer;
 
+import com.deep.tripease.dto.request.CustomerRequest;
+import com.deep.tripease.dto.response.CustomerResponse;
+import com.deep.tripease.model.Customer;
+
 public class CustomerDtoTransformer {
+
+    public static Customer CustomerRequestToCustomer(CustomerRequest customerRequest){
+        Customer saveCustomer=new Customer();
+        saveCustomer.setName(customerRequest.getName());
+        saveCustomer.setAge(customerRequest.getAge());
+        saveCustomer.setGender(customerRequest.getGender());
+        saveCustomer.setEmailId(customerRequest.getEmailId());
+        return saveCustomer;
+    }
+    public static CustomerResponse CustomerToCustomerResponse(Customer customer){
+
+        CustomerResponse customerResponse=new CustomerResponse();
+        customerResponse.setName(customer.getName());
+        customerResponse.setAge(customer.getAge());
+        customerResponse.setEmailId(customer.getEmailId());
+        return customerResponse;
+    }
 
 }
